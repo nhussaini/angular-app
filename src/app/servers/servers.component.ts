b0a1eeb784e563d019ccf89e9f76395bf52663b1
nhussaini <nasrullah_hussaini@yahoo.com>
2022-01-27 13:56:13 -0500

    two way databinding with ngModel

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No Server was created';
+  serverName = 'TestServer';
 
   constructor() {
     setTimeout(() => {
@@ -18,6 +19,11 @@ export class ServersComponent implements OnInit {
   ngOnInit(): void {}
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created';
+    this.serverCreationStatus =
+      'Server was created and name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 }
